c7aec8f11fcbd07cb10af5c35c940bf41bcc5638
Nurerva Yılmaz <130571315+ErvaYlmz@users.noreply.github.com>
2024-03-15 22:29:23 +0300

    Update activity4.php

--- in-classActivity4/activity4.php
+++ in-classActivity4/activity4.php
@@ -9,6 +9,59 @@
 
 <body>
 
+<?php 
+
+$input_money='';
+$input_currency='';
+$output_money='';
+$output_currency='';
+
+if ($_SERVER["REQUEST_METHOD"] == "GET") {
+    // Kullanıcı girdilerini al
+    if (isset($_GET['input'])) {
+        $input_money = $_GET['input'];
+    }
+
+    if (isset($_GET['currencies_in'])) {
+        $input_currency = $_GET['currencies_in'];
+    }
+
+    if (isset($_GET['currencies_out'])) {
+        $output_currency = $_GET['currencies_out'];
+    }
+
+    // convert fonksiyonunu çağır
+    $output_money = convert($input_money, $input_currency, $output_currency);
+}
+
+function convert($input_money, $input_currency, $output_currency){
+	
+        $value = $input_money;
+    
+	if($input_currency=='FUSD' && $output_currency=='TCAD'){ // usd to cad
+		$value= $value*135.40;
+	}
+	if($input_currency=='FUSD' && $output_currency=='TEUR'){ // usd to eur
+		$value= $value*91.82;
+	}
+	if($input_currency=='FCAD' && $output_currency=='TUSD'){ // cad to usd
+		$value= $value*73.86;
+	}
+	if($input_currency=='FCAD' && $output_currency=='TEUR'){ // cad to eur
+		$value= $value*67.82;
+	}
+	if($input_currency=='FEUR' && $output_currency=='TUSD'){ // eur to usd
+		$value= $value*108.89;
+	}
+	if($input_currency=='FEUR' && $output_currency=='TCAD'){ // eur to cad
+		$value= $value*147.44;
+	}
+	
+    return $value;
+}
+
+?>
+
 	<form action = "activity4.php" method="GET">
 		<table>
 			<tr>
@@ -16,16 +69,16 @@
 					From:
 				</td>
 				<td>
-					<input type="text" name="value"/>
+					<input type="text" name="input" value="<?php echo $input_money; ?>"/>
 				</td>
 				<td>
 					Currency:
 				</td>
 				<td>
-					<select name="currencies">
-						<option value="FUSD"/> USD </option>
-						<option value="FCAD"/> CAD </option>
-						<option value="FEUR"/> EUR </option>
+					<select name="currencies_in">
+						<option value="FUSD"<?php if ($input_currency === 'FUSD') echo ' selected'?>/> USD </option>
+						<option value="FCAD"<?php if ($input_currency === 'FCAD') echo ' selected'?>/> CAD </option>
+						<option value="FEUR"<?php if ($input_currency === 'FEUR') echo ' selected'?>/> EUR </option>
 					</select>
 				</td>	
 			</tr>
@@ -34,16 +87,16 @@
 					To:
 				</td>
 				<td>
-					<input type="text" name="value"/>
+					<input type="text" name="output" value="<?php echo $output_money; ?>"/>
 				</td>
 				<td>
 					Currency:
 				</td>
 				<td>
-					<select name="currencies">
-						<option value="TUSD"/> USD </option>
-						<option value="TCAD"/> CAD </option>
-						<option value="TEUR"/> EUR </option>
+					<select name="currencies_out">
+						<option value="TUSD"<?php if ($output_currency === 'TUSD') echo ' selected'?>/> USD </option>
+						<option value="TCAD"<?php if ($output_currency === 'TCAD') echo ' selected'?>/> CAD </option>
+						<option value="TEUR"<?php if ($output_currency === 'TEUR') echo ' selected'?>/> EUR </option>
 					</select>
 				</td>	
 			</tr>
